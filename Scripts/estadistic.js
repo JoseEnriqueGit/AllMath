1abce9defe3af2506d44aedca4b9cf22fdfa9672
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-06-18 19:52:51 -0400

    Funcion de la media, mediana y moda.

--- Scripts/estadistic.js
+++ Scripts/estadistic.js
@@ -17,11 +17,13 @@ function Average(list){
 // console.log(Average([5,7,6,4,3,8,7]));
 
 export function Mediana(list){
+   // Se organiza la lista de menor a mayor
     let organized = list.sort((a, b) => {
         return a - b;
     })
-    // Si el length de la lista es par
-    // return organized[5 - 1];
+   /* Si el length de la lista es par me quedara dos datos en el centro, por lo que tomo el primer dato del centro
+    con el length de la lista dividido entre dos y restadole uno, y luego tomo en segundo con el length de la lista 
+    dividido entre dos y luego esos dos datos los divido entre dos.*/
      if((organized.length % 2) == 0){
         return (organized[organized.length / 2 - 1] + organized[organized.length / 2]) / 2;
      }
@@ -29,35 +31,31 @@ export function Mediana(list){
         return organized[Math.round((organized.length / 2) - 1)];
      }
 }
-// console.log(Mediana([8,11,12,15,14,7,11,9,11]));
+// console.log(Mediana([1,2,3,4,5]));
 
 function Moda(list){
-   let numbers = Array.from(new Set(list));
+   // Convertimos la lista en un cojunto
+   let arrSet = Array.from(new Set(list));
+   // En repetidos guardo la cantidad de vece que se repite el numero de la lista Set.
    let repetidos = [];
-   let filtro;
-
-
-   for(let i = 0; i < numbers.length; i++){
-      filtro = list.filter((valor) =>{
-         return valor == numbers[i];
+   // En este for cuento la cantidad de veces que se repite un numero en la lista como parametro.
+   for(let i = 0; i < arrSet.length; i++){
+      let filter = list.filter((valor) =>{
+         return valor == arrSet[i];
       })
-      repetidos.push(filtro.length);
+      repetidos.push(filter.length);
    }
-//    let organized = repetidos;
-//    let a;
-//    a = organized.sort((a, b) => {
-//       return a - b;
-//   })
-//   let mayor = organized[organized.length - 1];
-   let mayor = 3;
+   // En mayor guardo el numero mas grande de la lista.
+   let mayor = Math.max(...Array.from(new Set(repetidos)));
 
-   var indices = [];
-   var idx = repetidos.indexOf(mayor);
+   // Aqui obtemos los indices de los numeros correspondiente a la moda
+   let indices = [];
+   let idx = repetidos.indexOf(mayor);
    while (idx != -1) {
      indices.push(idx);
      idx = repetidos.indexOf(mayor, idx + 1);
    }
 
-   return {numbers, repetidos, mayor, indices}
+   return {arrSet, repetidos, mayor, indices}
 }
-console.log(Moda([11,15,14,7,12,11,12,12,9,11]));
+console.log(Moda([11,15,14,7,7,12,11,7,12,12,9,11]));
