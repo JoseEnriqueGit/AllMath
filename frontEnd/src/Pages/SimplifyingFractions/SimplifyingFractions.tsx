4c9de62098a56c0256bdee38b4493926f65b48b1
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-02-25 22:50:04 -0400

    Create SimplifyingFraction components

---
+++ frontEnd/src/Pages/SimplifyingFractions/SimplifyingFractions.tsx
@@ -0,0 +1,9 @@
+function SimplifyingFractions() {
+  return (
+    <div className="App">
+      <p className="">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default SimplifyingFractions;
