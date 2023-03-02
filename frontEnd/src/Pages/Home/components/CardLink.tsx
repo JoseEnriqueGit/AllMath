c566a07334d66a0753abddf0c9b762ad5f88f38d
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-01 22:00:26 -0400

    Padding changed

--- frontEnd/src/Pages/Home/components/CardLink.tsx
+++ frontEnd/src/Pages/Home/components/CardLink.tsx
@@ -6,7 +6,7 @@ const CardLink = (props: { text: string; to: string }): JSX.Element => {
   return (
     <Link
       to={props.to}
-      className="flex flex-col bg-slate-700 rounded-md text-gray-200 font-medium ring-1 ring-slate-900 w-full p-3 gap-1 hover:bg-slate-600"
+      className="flex flex-col bg-slate-700 rounded-md text-gray-200 font-medium ring-1 ring-slate-900 w-full p-4 gap-1 hover:bg-slate-600"
     >
       <span className="">Aritmetica</span>
       <span className="font-bold text-lg">{props.text}</span>
