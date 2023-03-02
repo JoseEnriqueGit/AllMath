8bf006867dadda719e68bc828d2be3c50f7cb68a
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-02 09:52:58 -0400

    New component

---
+++ frontEnd/src/UI/Tiltle.tsx
@@ -0,0 +1,8 @@
+// Imports
+import { Link } from "react-router-dom";
+
+const Title = (): JSX.Element => {
+  return <Link to="/" className="col-span-full text-4xl font-bold">AllMath</Link>;
+};
+
+export default Title;
