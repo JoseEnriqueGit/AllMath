fac576c55d667dfada6a637b39be14ac82340380
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-04-11 18:18:27 -0400

    Style readjustment

--- frontEnd/src/UI/Tiltle.tsx
+++ frontEnd/src/UI/Tiltle.tsx
@@ -2,7 +2,7 @@
 import { Link } from "react-router-dom";
 
 const Title = (): JSX.Element => {
-  return <Link to="/" className="col-span-full text-4xl font-bold">AllMath</Link>;
+  return <Link to="/" className="flex self-start text-4xl font-bold">AllMath</Link>;
 };
 
 export default Title;
