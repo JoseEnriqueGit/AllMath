b07173547da5e8aa2a72a63f58678cfcc32aeeec
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-04-16 17:48:13 -0400

    Barrels

--- frontEnd/src/UI/index.ts
+++ frontEnd/src/UI/index.ts
@@ -1,4 +1,4 @@
 export * from "./Icons";
-export { default as SearchInput } from "./SearchInput/SearchInput";
+export { default as SearchInput } from "../Pages/Home/components/SearchInput/SearchInput";
 export { default as Tiltle } from "./Tiltle";
 export { default as Toggle } from "./Toggle";
